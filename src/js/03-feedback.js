866b0d04129cca49401b4b4ca0b3c8756cb4cabe
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-05-25 18:46:23 +0200

    Task 03
    Successfully completed task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+var throttle = require('lodash.throttle');
 
+// Selecting feedback form and saving it in variable
+const form = document.querySelector('.feedback-form');
+
+// Setting global variable for input data
+let inputData = {
+  email: form.email.value,
+  message: form.message.value,
+};
+
+// Initializing listener for feedback form, executing
+// throttled saving input data object in local storage
+form.addEventListener(
+  'input',
+  throttle(savingInputValues => {
+    inputData = {
+      email: form.email.value,
+      message: form.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify('inputData'));
+  }, 500),
+);
+
+// Initializing listener for feedback form submit event,
+// erasing form input and local storage
+form.addEventListener('submit', clearInputAndLocalStorage => {
+  clearInputAndLocalStorage.preventDefault();
+
+  console.log(inputData);
+
+  inputData = { email: '', message: '' };
+  form.email.value = '';
+  form.message.value = '';
+  localStorage.removeItem('feedback-form-state');
+});
